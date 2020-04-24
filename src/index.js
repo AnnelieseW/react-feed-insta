e1915bbd898b598aa203f753926e1edbe938970b
AnnelieseW <45906416+AnnelieseW@users.noreply.github.com>
2020-04-24 00:54:14 -0400

    version change

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import InstaFeed from "./lib";
 const App = () => (
   <div style={{ width: 640, margin: "15px auto" }}>
     <h1>Hello React</h1>
-      <InstaFeed name={'natgeotravel'} imgWidth={'200px'}/>
+      <InstaFeed name={'notjoeli'} imgWidth={'200px'}/>
   </div>
 );
 
